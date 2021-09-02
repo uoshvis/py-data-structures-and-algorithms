d34e4c29c3391a1ed6f5d2aa8a2d8cd65e5e3d40
uoshvis <uoshvis@gmail.com>
2021-09-02 14:42:52 +0300

    add utilities for balanced search tree subclasses

--- search_trees/treeMap.py
+++ search_trees/treeMap.py
@@ -52,6 +52,45 @@ class TreeMap(LinkedBinaryTree, MapBase):
     def _rebalance_access(self, p):
         pass
 
+    def _relink(self, parent, child, make_left_child):
+        """Relink parent node with child node (child can be None)"""
+        if make_left_child:     # make it a left child
+            parent._left = child
+        else:       # make it a right child
+            parent._right = child
+        if child is not None:       # make child point to parent
+            child._parent = parent
+
+    def _rotate(self, p):
+        """Rotate Position p above its parent"""
+        x = p._node
+        y = x._parent   # we assume this exits
+        z = y._parent   #  grandparent (possibly None)
+        if z is None:
+            self._root = x   # x becomes node
+            x._parent = None
+        else:
+            self._relink(z, x, y == z._left)    # x becomes a direct child of z\
+        # now rotate x and y, including transfer of middle subtree
+        if x == y._left:
+            self._relink(y, x._right, True)     # x._right becomes left child of y
+            self._relink(x, y, False)           # y becomes right child of x
+        else:
+            self._relink(y, x._left, False)     # x._left becomes right child of y
+            self._relink(x, y, True)            # y becomes left child of x
+
+    def _restructure(self, x):
+        """Perform trinode restructure of Position x with parent/grandparent"""
+        y = self.parent(x)
+        z = self.parent(y)
+        if x == self.right(y) == (y == self.right(z)): # matching alignments
+            self._rotate(y)     # single rotation (of y)
+            return y            # y is new subtree root
+        else:                   # opposite alignments
+            self._rotate(x)     # double rotation (of x)
+            self._rotate(x)
+            return x            # x is new subtree root
+
     # ------------------------------- navigational methods -------------------------------
     def first(self):
         """Return the first Position in the tree (or None if empty)."""
