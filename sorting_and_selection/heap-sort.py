7c4a6d84c0bb132b810e7fabd0cd9adfeb7b8e2d
uoshvis <uoshvis@gmail.com>
2021-09-09 07:17:42 +0300

    add notes

---
+++ sorting_and_selection/heap-sort.py
@@ -0,0 +1,2 @@
+# runs on O(n logn) in the worst case which is optimal for comparison-based sorting methods
+# however, heap-sort tends to be outperformed by both quick-sort and merge-sort on larger sequences.
