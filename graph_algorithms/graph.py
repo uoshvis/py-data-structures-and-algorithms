c85efb2789b7a4f40de0e63b8855075096506202
uoshvis <uoshvis@gmail.com>
2021-09-12 13:11:00 +0300

    add floyd-warshall algorithm

--- graph_algorithms/graph.py
+++ graph_algorithms/graph.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+
+
 class Graph:
     # ------------------------- nested Vertex class -------------------------
     class Vertex:
@@ -185,3 +188,22 @@ def BFS(g, s, discovered):
                     discovered[v] = e           # e is the tree edge that discovered v
                     next_level.append(v)        # v will be further considered in next pass
         level = next_level                      # relabel ’next’ level to become current
+
+
+# Floyd-Warshall
+def floyd_warshall(g):
+    """Return a new graph that is the transitive closure of g.
+    """
+    closure = deepcopy(g)
+    verts = list(closure.vertices())            # make indexable list
+    n = len(verts)
+    for k in range(n):
+        for i in range(n):
+            # verify that edge (i,k) exists in the partial closure
+            if i !=k and closure.get_edge(verts[i], verts[k]) is not None:
+                for j in range(n):
+                    # verify that edge (k,j) exists in the partial closure
+                    if i !=j !=k and closure.get_edge(verts[k], verts[j]) is not None:
+                        # if (i,j) not yet included, add it to the closure
+                        if closure.get_edge(verts[i], verts[j]) is None:
+                            closure.insert_edge(verts[i], verts[j])
