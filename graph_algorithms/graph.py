239e4f22860396f1fa58771c7195e9097843350c
uoshvis <uoshvis@gmail.com>
2021-09-12 12:55:13 +0300

    add dfs and bfs

--- graph_algorithms/graph.py
+++ graph_algorithms/graph.py
@@ -134,3 +134,54 @@ def run_DFS():
     # with None as its value.
     result = {u: None}
     DFS(g, u, result)
+
+
+# Function to reconstruct a directed path from u to
+# The function returns an ordered list of vertices on the path.
+def construct_path(u, v, discovered):
+    path = []
+    if v is discovered:
+        # we build list from v to u and then reverse it at the end
+        path.append(v)
+        walk = v
+        while walk is not u:
+            e = discovered[walk]                # find edge leading to walk
+            parent = e.opposite(walk)
+            path.append(parent)
+            walk = parent
+        path.reverse()                          # reorient path from u to v
+    return path
+
+
+# Top-level function that returns a DFS forest for an entire graph
+def DFS_complete(g):
+    """Perform DFS for entire graph and return forest as a dictionary.
+    Result maps each vertex v to the edge that was used to discover it.
+    (Vertices that are roots of a DFS tree are mapped to None.)
+    """
+    forest = {}
+    for u in g.vertices():
+        if u not in forest:
+            forest[u] = None            # u will be the root of a tree
+            DFS(g, u, forest)
+    return forest
+
+
+# Breadth-first search
+def BFS(g, s, discovered):
+    """Perform BFS of the undiscovered portion of Graph g starting at Vertex s.
+
+    discovered is a dictionary mapping each vertex to the edge that was used to
+    discover it during the BFS (s should be mapped to None prior to the call).
+    Newly discovered vertices will be added to the dictionary as a result.
+    """
+    level = [s]                                 # first level includes only s
+    while len(level) > 0:
+        next_level = []                         # prepare to gather newly found vertices
+        for u in level:
+            for e in g.incident_edges(u):       # for every outgoing edge from u
+                v = e.opposite(u)
+                if v not in discovered:         # v is an unvisited vertex
+                    discovered[v] = e           # e is the tree edge that discovered v
+                    next_level.append(v)        # v will be further considered in next pass
+        level = next_level                      # relabel ’next’ level to become current
