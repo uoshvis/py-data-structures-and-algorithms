1100dc7c155016f810975c69dd9f4e4bb0dbee03
uoshvis <uoshvis@gmail.com>
2021-09-13 21:29:14 +0300

    add Kruskal minimum spanning tree with Partition union data structure

--- graph_algorithms/graph.py
+++ graph_algorithms/graph.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 from priority_queues.adaptable_heap_priority_queue import AdaptableHeapPriorityQueue
+from priority_queues.heap_priority_queue import HeapPriorityQueue
 
 
 class Graph:
@@ -332,3 +333,76 @@ def MST_PrimJarnik(g):
                 if wgt < d[v]:                  # better edge to v?
                     d[v] = wgt                  # update the distance
                     pq.update(pqlocator[v], d[v], (v, link))    # update the pq entry
+
+
+#  Kruskal’s algorithm
+#  Running time O(m log n)
+
+def MST_Kruskal(g):
+    """Compute a minimum spanning tree of a graph using Kruskal s algorithm.
+
+    Return a list of edges that comprise the MST.
+    The elements of the graph s edges are assumed to be weights.
+    """
+    tree = []                   # list of edges in spanning tree
+    pq = HeapPriorityQueue()    # entries are edges in G, with weights as key
+    forest = Partition()        # keeps track of forest clusters
+    position = {}               # map each node to its Partition entry
+
+    for v in g.vertices():
+        position[v] = forest.make_group(v)
+
+    for e in g.edges():
+        pq.add(e.elemet(), e)               # edge’s element is assumed to be its weight
+
+    size = g.vertex_count()
+    while len(tree) != size - 1 and not pq.is_empty():
+        # tree not spanning and unprocessed edges remain
+        weight, edge = pq.remove_min()
+        u, v = edge.endpoints()
+        a = forest.find(position[u])
+        b = forest.find(position[v])
+        if a != b:
+            tree.append(edge)
+            forest.union(a, b)
+
+
+class Partition:
+    """Union-find structure for maintaining disjoint sets."""
+
+    # ------------------------- nested Position class -------------------------
+    class Position:
+        __slots__ = '_container', '_element', '_size', '_parent'
+
+        def __init__(self, container, e):
+            """Create a new position that is the leader of its own group."""
+            self._container = container             # reference to Partition instance
+            self._element = e
+            self._size = 1
+            self._parent = self                     # convention for a group leader
+
+        def element(self):
+            return self._element
+
+    # ------------------------- public Partition methods -------------------------
+    def make_group(self, e):
+        """Makes a new group containing element e, and returns its Position."""
+        return self.Position(self, e)
+
+    def find(self, p):
+        """ Finds the group containing p and return the position of its leader."""
+        if p._parent != p:
+            p._parent = self.find(p._parent)        # overwrite p. parent after recursion
+        return p._parent
+
+    def union(self, p, q):
+        """ Merges the groups containing elements p and q (if distinct). """
+        a = self.find(p)
+        b =self.find(q)
+        if a is not b:                      # only merge if different groups
+            if a._size > b._size:
+                b._parent = a
+                a._size += b._size
+            else:
+                a._parent = b
+                b._size += a._size
